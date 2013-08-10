727a89c2f9a59d1087f05034153af51f1992e168
Piotr Fidkowski <piotr@fidkowski.com>
2013-08-09 17:38:55 -0700

    added struct to wrap around a set of parameters

--- shader/quasicrystal.cc
+++ shader/quasicrystal.cc
@@ -29,23 +29,62 @@ DEFINE_int32(width, 600, "Width of output image.");
 DEFINE_int32(height, 600, "Height of output image.");
 DEFINE_string(shader_source, "qc.frag",
               "Path to fragment shader source code");
-DEFINE_int32(num_waves, 7, "Initial number of waves.");
-DEFINE_double(spatial_freq, 1.0 / 5.0, "Initial spatial frequency of waves.");
-DEFINE_string(phases,
+DEFINE_int32(num_waves, 7, "Initial number of waves, max of 15.");
+DEFINE_double(wavenumber, 0.2, "Initial spatial frequency of waves.");
+DEFINE_string(angular_frequencies,
               "1.0, 0.9, 0.8, 0.7, 0.6,"
               "0.5, 0.4, 0.3, 0.2, 0.1,"
               "0.1, 0.2, 0.3, 0.4, 0.5",
-              "Comma seperated list of phases, maximum of 15.");
+              "Comma seperated list of initial wave angular frequencies.");
 DEFINE_double(time_granularity, 0.01,
               "Parameter that controls granularity in modifying the speed.");
 
 // Keep in sync with constant in qc.frag
 const int kMaxNumWaves = 15;
 
+namespace {
+
+// A sufficient set of parameters to describe any snapshot of the quasicrystals.
+struct QCParams {
+  QCParams()
+      : t(0.0),
+        num_waves(1),
+        mix(0.0),
+        angular_frequencies({1.0}),
+        wavenumber(0.2) {}
+  // Current time in the wave propagation.
+  float t;
+  // Number of waves.
+  int num_waves;
+  // Wave number mixing parameter, 0 = num_waves, 1 = num_waves + 1
+  float mix;
+  // Angular frequencies of each wave, individually specified.
+  float angular_frequencies[kMaxNumWaves];
+  // Spatial frequency of all of the waves.
+  // TODO(piotrf): specify per wave wavenumbers.
+  float wavenumber;
+};
+
+QCParams InitQCParamsFromFlags() {
+  QCParams params;
+  params.num_waves = std::max(std::min(FLAGS_num_waves, kMaxNumWaves), 1);
+  params.wavenumber = FLAGS_wavenumber;
+  // Split comma seperated string into floats.  I should just use boost...
+  std::stringstream ss(FLAGS_angular_frequencies);
+  std::string phase_str;
+  int i = 0;
+  while (std::getline(ss, phase_str, ',') && i < kMaxNumWaves) {
+    params.angular_frequencies[i] = atof(phase_str.c_str());
+    ++i;
+  }
+  return params;
+}
+
 class QCWindow : public graphics::Window2d {
  public:
-  QCWindow(int width, int height) :
-      Window2d(width, height, "quasicrystal") {
+  QCWindow(int width, int height, const QCParams& params)
+      : Window2d(width, height, "quasicrystal"),
+        params_(params) {
   }
 
  protected:
@@ -70,24 +109,11 @@ class QCWindow : public graphics::Window2d {
       return false;
     }
 
-    // Initialize parameters.
-    num_waves_ = std::max(std::min(FLAGS_num_waves, kMaxNumWaves), 1);
-    spatial_freq_ = FLAGS_spatial_freq;
-
-    t_ = 0.0;
+    // Initialize any simulation variables outside of params.
+    is_paused_ = false;
     dt_ = 5 * FLAGS_time_granularity;
-    mix_ = 0.0; mixv_ = 0.0;
+    mixv_ = 0.0;
 
-    // Split comma seperated string into floats.  I should just use boost...
-    GLfloat* phases = new GLfloat[kMaxNumWaves];
-    std::stringstream ss(FLAGS_phases);
-    std::string phase_str;
-    int i = 0;
-    while (std::getline(ss, phase_str, ',') && i < kMaxNumWaves) {
-      phases[i] = atof(phase_str.c_str());
-      ++i;
-    }
-  
     // Pass phases into the shader via a 1-d texture.  
     glEnable(GL_TEXTURE_1D);
     glGenTextures(1, &phases_texture_);
@@ -100,45 +126,42 @@ class QCWindow : public graphics::Window2d {
                  0,             // border
                  GL_RED,        // pixel data format
                  GL_FLOAT,      // pixel data type
-                 phases);
+                 params_.angular_frequencies);
   
     // I think this should setup sampling to be close enough to selecting
     // values from the array.  I haven't thoroughly tested.
     glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  
-    delete[] phases;
 
-    is_paused_ = false;
     return true;
   }
   
   virtual void Draw() {
     if (!is_paused_) {
-      t_ += dt_;
+      params_.t += dt_;
 
-      mix_ += mixv_;
-      if (mix_ < 0) {
-        mix_ = 0.0;
+      params_.mix += mixv_;
+      if (params_.mix < 0) {
+        params_.mix = 0.0;
         mixv_ = 0.0;
       }
-      if (mix_ > 1) {
-        mix_ = 0.0;
+      if (params_.mix > 1) {
+        params_.mix = 0.0;
         mixv_ = 0.0;
-        ++num_waves_;
+        ++params_.num_waves;
       }
     }
   
     // Pass in all parameters to the shader.
     GLint t_loc = glGetUniformLocation(shader_, "t");
-    glUniform1f(t_loc, t_);
+    glUniform1f(t_loc, params_.t);
     GLint num_waves_loc = glGetUniformLocation(shader_, "num_waves");
-    glUniform1i(num_waves_loc, num_waves_);
+    glUniform1i(num_waves_loc, params_.num_waves);
     GLint spatial_freq_loc = glGetUniformLocation(shader_, "spatial_freq");
-    glUniform1f(spatial_freq_loc, spatial_freq_);
+    glUniform1f(spatial_freq_loc, params_.wavenumber);
     GLint mix_loc = glGetUniformLocation(shader_, "mix");
-    glUniform1f(mix_loc, mix_);
+    glUniform1f(mix_loc, params_.mix);
     GLint phases_loc = glGetUniformLocation(shader_, "phases");
     glUniform1i(phases_loc, 0);
     // Update resolution in shader.
@@ -167,20 +190,20 @@ class QCWindow : public graphics::Window2d {
         if (mixv_ > 0.0) {
           mixv_ = -0.01;
         } else if (mixv_ < 0.0) {
-          mix_ = 0.0; mixv_ = 0.0;
-        } else if (num_waves_ > 1) {
-          --num_waves_;
-          mix_ = 1.0; mixv_ = -0.01;
+          params_.mix = 0.0; mixv_ = 0.0;
+        } else if (params_.num_waves > 1) {
+          --params_.num_waves;
+          params_.mix = 1.0; mixv_ = -0.01;
         }
         break;
       case XK_bracketright:
         if (mixv_ < 0.0) {
           mixv_ = 0.01;
         } else if (mixv_ > 0.0) {
-          mix_ = 0.0; mixv_ = 0.0;
-          ++num_waves_;
-        } else if (num_waves_ < kMaxNumWaves - 1) {
-          mix_ = 0.0; mixv_ = 0.01;
+          params_.mix = 0.0; mixv_ = 0.0;
+          ++params_.num_waves;
+        } else if (params_.num_waves < kMaxNumWaves - 1) {
+          params_.mix = 0.0; mixv_ = 0.01;
         }
         break;
       case XK_period:
@@ -193,10 +216,10 @@ class QCWindow : public graphics::Window2d {
         is_paused_ = !is_paused_;
         break;
       case XK_minus:
-        spatial_freq_ *= 1.1;
+        params_.wavenumber *= 1.1;
         break;
       case XK_equal:
-        spatial_freq_ /= 1.1;
+        params_.wavenumber /= 1.1;
         break;
       case XK_Escape:
         Close();
@@ -209,17 +232,17 @@ class QCWindow : public graphics::Window2d {
  private:
   GLuint shader_;
   GLuint phases_texture_;
-  float t_, dt_;
   bool is_paused_;
-  int num_waves_;
-  float spatial_freq_;
-  float mix_, mixv_;
+  float dt_, mixv_;
+  QCParams params_;
 };
+
+}  // namespace
 
 int main(int argc, char **argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  QCWindow window(FLAGS_width, FLAGS_height);
+  QCWindow window(FLAGS_width, FLAGS_height, InitQCParamsFromFlags());
   window.Run();
   
   return EXIT_SUCCESS;
